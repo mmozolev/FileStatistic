2a76c44db4bd12d19d1a20b4f895745c34db85eb
Мозолев <mozolevwork@gmail.com>
2021-02-01 20:26:55 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import fileStatistic.FileStatistic;
+
+public class Main {
+    public static void main(String[] args) {
+        FileStatistic fileStatistic = new FileStatistic();
+        fileStatistic.printMaxFreqWord();
+    }
+}
